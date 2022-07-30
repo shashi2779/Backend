65453c675e921a56052c939f2b7d46e3d7244e81
shashi2779 <yadavshashikant2779@gmail.com>
2022-07-30 09:33:13 +0530

    first commit

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+# install
+
+1- npm init -y
+
+2- npm install express
+
+3- npm i -g nodemon
+*add "start":"nodemon server.js" to package.json
+
+//if you want to accept data in backend then use it with post route.
+app.use(express.json())
+
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+//require kiye
+const express = require("express")
+
+//call kiye
+const app = express();
+//if you want to accept data in backend then use it with post route.
+app.use(express.json())
+
+app.get("/sayhello",function(req,res){
+    res.end("get route se msg")
+})
+
+app.post("/sayhello",function(req,res){
+    console.log("data",req.body)     // backend prr output
+    res.end("hello from post route") // frontend prr output(postman)
+})
+
+app.get("/sayhello/:num",function(req,res){
+    console.log("data",req.params.num);
+    let n = req.params.num;
+    let sq = req.params.num*req.params.num;
+    res.end(sq+" ")
+})
+
+app.get("/sayhello/:num1/:num2",function(req,res){
+    console.log("param1 se data",req.params.num1)
+    console.log("param2 se data",req.params.num2)
+    
+    let sq = req.params.num1*req.params.num2;
+
+    res.end(sq+" ") 
+
+})
+
+
+//listen kiye
+app.listen(3000,function(req,res){
+    console.log("server running on 3000 route id")
+})
