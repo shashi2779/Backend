1063122b5051710e9743d81fccccebd2c5aa1bfe
shashi2779 <yadavshashikant2779@gmail.com>
2023-03-30 20:43:07 +0530

    fghr

--- backend/api.js
+++ backend/api.js
@@ -4,10 +4,6 @@ const app = express();
 
 const cookieParser = require("cookie-parser")
 
-
-
-
-
 const userRouter = require("./routes/userRoutes")
 const authRouter = require("./routes/authRoutes")
 
@@ -18,10 +14,12 @@ app.use(express.json())
 // call kiye
 app.use(cookieParser())
 
-
-
 app.use("api/v1/auth",authRouter)
 app.use("api/v1/auth",userRouter)
+
+// update user profile
+//delete user profile
+
 
 app.listen(3000, function () {
   console.log("server running on 3000 port")
