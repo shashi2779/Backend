2ed4b0c54db7686a111c8a840fd9fe842e1135d9
shashi2779 <yadavshashikant2779@gmail.com>
2023-05-04 17:34:53 +0530

    sdcsd

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -24,7 +24,7 @@ const secrets = require("../secrets")
         })
     }
   }
-  
+
   
   async function loginController(req, res) {
     try {
--- backend/secrets.js
+++ backend/secrets.js
@@ -1,6 +1,7 @@
 module.exports={
     JWTSECRET:"qwertyasdgh",
-    DB_LINK : "mongodb+srv://yadavshashi:Ief8kvPHtozTckmj@freecluster.bmcxj8d.mongodb.net/?retryWrites=true&w=majority",
+    DB_LINK : "mongodb+srv://yadavshashi:SUW3BnYAJ3vZjJz8@freecluster.bmcxj8d.mongodb.net/?retryWrites=true&w=majority",
     APP_PASSWORD : "flhgqrcakvcgjobn",
     APP_EMAIL : "yadavshashikant2779@gmail.com"
 }
+
